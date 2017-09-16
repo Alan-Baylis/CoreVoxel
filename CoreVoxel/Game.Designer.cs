29df0b45442d99113d82118833176704c7771e58
Li Smith <lrhude.elijah@gmail.com>
2017-09-16 18:51:56 -0400

    Test

--- CoreVoxel/Game.Designer.cs
+++ CoreVoxel/Game.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.toolCanvas = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toolCanvas)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.canvas.Location = new System.Drawing.Point(257, 27);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1130, 740);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // toolCanvas
+            // 
+            this.toolCanvas.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolCanvas.Location = new System.Drawing.Point(0, 27);
+            this.toolCanvas.Name = "toolCanvas";
+            this.toolCanvas.Size = new System.Drawing.Size(260, 740);
+            this.toolCanvas.TabIndex = 1;
+            this.toolCanvas.TabStop = false;
+            this.toolCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.toolCanvas_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1387, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 16;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1152, 673);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1387, 764);
+            this.Controls.Add(this.toolCanvas);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Game";
             this.Text = "CoreVoxel";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toolCanvas)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.PictureBox toolCanvas;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- CoreVoxel/Game.cs
+++ CoreVoxel/Game.cs
@@ -1,20 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CoreVoxel
 {
     public partial class Game : Form
     {
+        //define global variables
+        public int[] cameraPosition { get; set; } = { 0, 0, 0 };
+
+        public int gridSize { get; } = 30;
+
+        //constructor
         public Game()
         {
             InitializeComponent();
+
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            timer.Start();
+        }
+
+        //update canvas
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            canvas.Refresh();
+            toolCanvas.Refresh();
+
+            cameraPosition[2]++;
+        }
+
+        //main canvas render
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = canvas.Width;
+            int height = canvas.Height;
+
+            //define some variables for camera position
+            float cameraAngleX = 0;
+            float cameraAngleY = 0;
+            float cameraAngleZ = 0;
+
+            //draw grid
+            int size = 22;
+
+            int baseX = width / 2 - (gridSize * size) / 2;
+            int baseY = height / 2 - (gridSize * size) / 2;
+            int baseZ = 0;
+
+            int cameraAngleOffsetX = 0;
+            int cameraAngleOffsetY = 0;
+            int cameraAngleOffsetZ = 0;
+
+            for (int x = 0; x < gridSize * size; x += size)
+            {
+                cameraAngleOffsetX++;
+                cameraAngleX = (float)(Math.Cos(cameraPosition[0] + cameraAngleOffsetX) * 2);
+
+                cameraAngleOffsetY = 0;
+                for (int y = 0; y < gridSize * size; y += size)
+                {
+                    cameraAngleOffsetY++;
+                    cameraAngleY = (float)(Math.Sin(cameraPosition[1] + cameraAngleOffsetY) * 2);
+
+                    g.DrawRectangle(Pens.Black, baseX + x + cameraAngleX, baseY + y + cameraAngleY, size - cameraAngleZ, size - cameraAngleZ);
+                }
+            }
+        }
+
+        //tool canvas render
+        private void toolCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
